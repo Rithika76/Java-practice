ac5bbc5019848f50f4512b47226edf45561b8bd4
Rithika76 <rithisamyu26@gmail.com>
2026-08-11 19:17:30 +0530

    validdate.java

--- CLASS WORK/10 AUG/valid date.java
+++ CLASS WORK/10 AUG/valid date.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int yr = sc.nextInt();
         int mth = sc.nextInt();
         int day = sc.nextInt();
-
         if (yr >= 1000 && yr <= 9999) {
             if (mth >= 1 && mth <= 12) {
                 int max;
-
                 if (mth == 2) {
                     if ((yr % 4 == 0 && yr % 100 != 0) || (yr % 400 == 0)) {
                         max = 29;
@@ -23,7 +20,6 @@ public class Main {
                 } else {
                     max = 31;
                 }
-
                 if (day >= 1 && day <= max) {
                     System.out.println("Valid");
                 } else {
@@ -35,7 +31,5 @@ public class Main {
         } else {
             System.out.println("Invalid");
         }
-
-        sc.close();
     }
 }
